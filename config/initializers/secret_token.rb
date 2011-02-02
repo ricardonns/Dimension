cb244db7d1e45ef216d8594b12a4c59af07f46ee
Ricardo Nunes <ricardonns@gmail.com>
2011-02-02 22:41:59 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dimension::Application.config.secret_token = '5d1b28c2379097b4e07345f93246002a716882af72f8cd092d3d2438d9fd1fec13eb028b3fb35d347ea3ab1aebd4722a4b41163d0ce107bcb3f025ab57a543c8'
